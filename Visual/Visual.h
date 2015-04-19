31b94b254c6be2f44a169be3ac6d82960670d3e1
Ryan Quan <ryanhquan@gmail.com>
2015-04-18 21:28:26 -0700

    Initial Commit

---
+++ Visual/Visual.h
@@ -0,0 +1,13 @@
+//
+//  Visual.h
+//  Visual
+//
+//  Created by Ryan Quan on 4/18/15.
+//  Copyright (c) 2015 Ryan Quan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Visual : NSObject
+
+@end
